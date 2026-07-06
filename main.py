5c357e562e48d9b441f82fe51861823cbdba1b54
Josh19218 <jumpingjosh4442@gmail.com>
2026-07-06 19:40:00 +0100

    Connect SQLite database using SQLAlchemy

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
+
+from database import engine, SessionLocal, Base
+import models
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
-class Expense(BaseModel):
+class ExpenseCreate(BaseModel):
   amount: float
   category: str
   description: str
 
-expenses = []
-
-@app.post("/expenses")
-def add_expense(expense: Expense):
-  expenses.append(expense)
-  return expense
-
-@app.get("/expenses")
-def get_expenses():
-  return expenses
-
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
+  
 @app.get("/")
 def read_root():
   return{"message": "Expense Tracker API is running"}
+
+@app.post("/expenses")
+def add_expense(expense: ExpenseCreate, db: Session = Depends(get_db)):
+  db_expense = models.Expense(
+    amount=expense.amount,
+    category=expense.category,
+    description=expense.description
+  )
+  db.add(db_expense)
+  db.commit()
+  db.refresh(db_expense)
+  return db_expense
+
+@app.get("/expenses")
+def get_expenses(db: Session = Depends(get_db)):
+  return db.query(models.Expense).all()
