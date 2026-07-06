cfc764dafb9202f4c9e5a51b7f1c29fc16b8e090
Josh19218 <jumpingjosh4442@gmail.com>
2026-07-06 19:13:41 +0100

    Add POST and GET endpoints for expenses

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
+
+class Expense(BaseModel):
+  amount: float
+  category: str
+  description: str
+
+expenses = []
+
+@app.post("/expenses")
+def add_expense(expense: Expense):
+  expenses.append(expense)
+  return expense
+
+@app.get("/expenses")
+def get_expenses():
+  return expenses
 
 @app.get("/")
 def read_root():
